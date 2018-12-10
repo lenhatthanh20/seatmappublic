16ee36fb76668b1ae062e9dfb2aa5e75f9cfd8fa
lenhatthanh20 <lenhatthanh20@gmail.com>
2018-12-10 13:26:39 +0700

    add index.php and index.tpl

---
+++ controllers/index.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once('libs/custom/smarty/smartyConfig.php');
+require_once('models/Profile.php');
+require_once('models/Seatmap.php');
+
+
+$profile = new Profile();
+$seatmap = new Seatmap();
+
+/* List All Users */
+$arrayAllProfile = $profile->listAllProfile();
+
+/* List All Seatmap */
+$arrayAllSeatmap = $seatmap->listAllSeatmap();
+//var_dump($arrayAllUser);
+$smarty->assign('arrayAllProfile', $arrayAllProfile);
+$smarty->assign('arrayAllSeatmap', $arrayAllSeatmap);
+
+$smarty->display('index.tpl');
--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-//session_start();
-//require_once('controllers/login.php');
-//require_once('controllers/register.php');
-
-echo "Homepage ";
