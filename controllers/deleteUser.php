652e16d4f4767dabb30e9552d5f671dca3b7106c
lenhatthanh20 <lenhatthanh20@gmail.com>
2018-12-17 15:37:46 +0700

    Refactor deleteUser.php

--- controllers/deleteUser.php
+++ controllers/deleteUser.php
@@ -1,35 +1,33 @@
 <?php
 // Start the session
 session_start();
+
 require_once('../models/Profile.php');
+require_once('../models/Utility.php');
 
 $profile = new Profile();
+$utility = new Utility();
 
 /* Check authentication by session */
-if( isset($_SESSION["username"])) {
-    //$smarty->assign('username', $_SESSION["username"]);
-}else {
-    header('Location: /seatmap/controllers/index.php');
-    die();
+if( !isset($_SESSION["username"])) {
+    $utility->redirect('/seatMap/controllers/index.php');
 }
 
 if (isset($_POST['id']) && isset($_POST['path'])) {
 
-    $id = $_POST['id'];
-    $imagePath = $_POST['path'];
+    $id = htmlspecialchars($_POST['id']);
+    $imagePath = htmlspecialchars($_POST['path']);
 
     $success = $profile->deleteProfile($id);
 
     if($success) {
-        //Delete image
         if($imagePath !== "../images/default.png"){
             unlink ($imagePath);
-            $message = 'Profile is deleted successfully!';
         }
+        echo true;
     } else {
-        $message = 'Can not delete this user!';
+        echo false;
     }
-
 
     header('Location: dashboard.php');
 }
